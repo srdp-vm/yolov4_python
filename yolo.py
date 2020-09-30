7ca9f6dcc50ef8d3729453fb8e663cdec35a2d02
Nisces <nisces0304@outlook.com>
2020-09-30 17:23:49 +0800

    fix bug

--- yolo.py
+++ yolo.py
@@ -145,6 +145,6 @@ class YOLO:
         detections = dk.detect_image(self.net, self.class_names, self.darknet_image, self.thresh)
         for detection in detections:
             pred_class, score, box = detection
-            count = items_count.get(pred_class, default=0)
+            count = items_count.get(pred_class, 0)
             items_count[pred_class] = count
         return items_count
